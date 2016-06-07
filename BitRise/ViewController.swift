076816cda5994d838db983aa3b62136e43059ef9
Soni, Ankita (US - Mumbai) <anksoni@deloitte.com>
2016-06-07 13:55:54 +0530

    Checking if build gets created on pull request

--- BitRise/ViewController.swift
+++ BitRise/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Checking if build is created on pull request");
     }
 
     override func didReceiveMemoryWarning() {
