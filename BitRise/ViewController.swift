4c9ba038d499e37d5689f9a4b433b4999f909eaa
Soni, Ankita (US - Mumbai) <anksoni@deloitte.com>
2016-06-07 15:00:33 +0530

    Try 2

--- BitRise/ViewController.swift
+++ BitRise/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Checking if build is created on pull request");
+        print("Checking if build is created on pull request part 2");
     }
 
     override func didReceiveMemoryWarning() {
